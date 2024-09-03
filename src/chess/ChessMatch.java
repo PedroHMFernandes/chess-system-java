5e87ee1ee2173a8b5a08f337602c73f4b86baf52
Pedro Fernandes <pedrovosvisk10@gmail.com>
2024-09-02 21:39:05 -0300

    Piece move count

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -63,6 +63,7 @@ public class ChessMatch {
 
 		if (testCheck(currentPlayer)) {
 			undoMove(source, target, capturedPiece);
+			
 			throw new ChessException("You can't put yourself in check");
 		}
 
@@ -71,7 +72,7 @@ public class ChessMatch {
 		if (testCheckMate(opponent(currentPlayer))) {
 			checkMate = true;
 		} else {
-
+			
 			nextTurn();
 		}
 		return (ChessPiece) capturedPiece;
@@ -84,7 +85,8 @@ public class ChessMatch {
 	}
 
 	private Piece makeMove(Position source, Position target) {
-		Piece p = board.removePiece(source);
+		ChessPiece p = (ChessPiece) board.removePiece(source);
+		p.increaseMoveCount();
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
 		if (capturedPiece != null) {
@@ -95,7 +97,8 @@ public class ChessMatch {
 	}
 
 	private void undoMove(Position source, Position target, Piece capturedPiece) {
-		Piece p = board.removePiece(target);
+		ChessPiece p =  (ChessPiece) board.removePiece(target);
+		p.decreaseMoveCount();
 		board.placePiece(p, source);
 		if (capturedPiece != null) {
 			board.placePiece(capturedPiece, target);
