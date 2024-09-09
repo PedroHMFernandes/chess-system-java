258422d1a28653167667a3e1f05a97153b15279d
Pedro Fernandes <pedrovosvisk10@gmail.com>
2024-09-09 17:53:49 -0300

    Special move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -37,6 +37,15 @@ public class Program {
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
+				if (chessMatch.getPromoted() != null) {
+					System.out.print("Enter piece for promotion(B/N/R/Q): ");
+					String type = sc.nextLine();
+					while (!type.equals("B") && !type.equals("Q") && !type.equals("N") && !type.equals("R")) {
+						System.out.print("Invalid value! Enter piece for promotion(B/N/R/Q): ");
+						type = sc.nextLine();
+					}
+					chessMatch.replacePromotedPiece(type);
+				}
 			} catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
